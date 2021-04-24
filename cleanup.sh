70196c73f500501fafdf6a953336e8a819e6aa21
SamSamhuns <samhunsadamant@gmail.com>
2021-04-24 19:30:53 +0400

    del extra train,test code; add train args; fix comments

--- cleanup.sh
+++ cleanup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 echo "Removing all .DS_Store files"
-
-# Delete all .DS_Store files
 find . -type f -name '*.DS_Store' -delete
 find . -type f -name '*._.DS_Store' -delete
 
@@ -10,8 +8,7 @@ echo "Removing python build artifacts"
 # + at the end means the cmd is executed for all file/dir with matches
 find . -type d -name "__pycache__" -exec rm -r "{}" +
 
-echo "Removing pytorch training logs & experiments"
-# Remove build artifacts
+echo "Removing pytorch training experiment log artifacts & logs"
 rm -rf experiments*
 rm -rf logs*
 rm -rf .data_cache
