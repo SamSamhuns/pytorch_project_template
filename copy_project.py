1e1b85a7128a96c95f032f22fd691f4628c1f17d
SamSamhuns <samhunsadamant@gmail.com>
2021-07-21 10:29:29 +0400

    Fix debug print in copy project

--- copy_project.py
+++ copy_project.py
@@ -10,13 +10,14 @@ from shutil import copytree, ignore_patterns
 current_dir = Path()
 assert (current_dir / 'copy_project.py').is_file(
 ), 'Script should be executed in the pytorch-project-template directory'
-assert len(sys.argv) == 2, 'Specify a name for the new project. Example: python3 new_project.py MyNewProject'
+assert len(sys.argv) == 2, 'Specify a name for the new project. Example: python3 copy_project.py DIR_PATH_TO_NEW_PROJECT'
 
 project_name = Path(sys.argv[1])
 target_dir = current_dir / project_name
 
-ignore = [".git", "data", "copy_project.py"
-          "LICENSE", "README.md", "__pycache__"]
+ignore = [".git", "data", "copy_project.py", "venv*",
+          "logs*", "checkpoints*", "experiments*",
+          "__pycache__", ".DS_Store"]
 
 copytree(current_dir, target_dir, ignore=ignore_patterns(*ignore))
 print('New project initialized at', target_dir.absolute().resolve())
