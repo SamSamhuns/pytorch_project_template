1b63e886849950354790c02d452c8d2449335fda
SamSamhuns <samhunsadamant@gmail.com>
2022-05-20 10:00:55 +0400

    Add argparse fixes

--- test.py
+++ test.py
@@ -6,13 +6,11 @@ import argparse
 def get_parsed_args():
     parser = argparse.ArgumentParser(
         description='PyTorch Test. Currently only supports image classification')
-    parser.add_argument('-c', '--config_file',
-                        default="configs/mnist_config.py",
-                        help='Config file for agent.\n' +
-                        'Default: %(default)s')
-    parser.add_argument('-ckpt', '--checkpoint_file',
-                        help='Path to checkpoint file.\n' +
-                        'Default: %(default)s')
+    parser.add_argument("-c", "--config_file", dest="config_file",
+                        default="configs/classifier_cpu_config.py",
+                        help="Config file for agent. default: %(default)s")
+    parser.add_argument("--ckpt", "--checkpoint_file", dest="checkpoint_file",
+                        help="Path to checkpoint file. default: %(default)s")
     args = parser.parse_args()
     # remove / and .py from config path
     args.config_file = args.config_file.replace('/', '.')[:-3]
