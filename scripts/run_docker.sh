ce62118a0229706932af9b833899304a8916055f
SamSamhuns <samhunsadamant@gmail.com>
2022-11-16 14:25:59 +0400

    Update server reqs

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -4,6 +4,7 @@ docker run \
       -ti --rm \
       -p 0.0.0.0:6006:6006 \
       -v "$PWD"/checkpoints_docker:/pytorch_model/checkpoints \
+      -v "$PWD"/data:/pytorch_model/data \
       --name pytorch_container \
       pytorch_model \
       bash
