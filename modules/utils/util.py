c8c481ab54a0c4d48469b93c5a95dd8e62dc7c7b
SamSamhuns <samhunsadamant@gmail.com>
2023-01-31 21:03:09 +0400

    Add color print & open port chk funcs

--- modules/utils/util.py
+++ modules/utils/util.py
@@ -4,9 +4,35 @@ this util.py contains utility funcs meant to be used internally & externally
 import os
 import json
 import glob
+import socket
 import functools
 from collections import OrderedDict
 from easydict import EasyDict as edict
+
+
+class BColors:
+    """
+    Border Color values for pretty printing in terminal
+    Sample Use:
+        print(f"{BColors.WARNING}Warning: Information.{BColors.ENDC}"
+    """
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def is_port_in_use(port: int) -> bool:
+    """
+    Checks if a port is free for use
+    """
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as stream:
+        return stream.connect_ex(('localhost', int(port))) == 0
 
 
 class AverageMeter(object):
@@ -31,7 +57,6 @@ class AverageMeter(object):
 class MissingConfigError(Exception):
     """Raised when configurations are missing
     """
-    pass
 
 
 def identity(x):
