b79892868e09f313cfcda62bb3d98796ff311158
SamSamhuns <samhunsadamant@gmail.com>
2023-01-31 16:14:36 +0400

    Fix proper logger prints

--- modules/utils/statistics.py
+++ modules/utils/statistics.py
@@ -1,26 +1,26 @@
+import sys
 import logging
+from subprocess import call
+
+import torch
 
 
 def print_cuda_statistics():
     logger = logging.getLogger("Cuda Statistics")
 
-    import sys
-    import torch
-    from subprocess import call
 
-    logger.info('__Python VERSION:  {}'.format(sys.version))
-    logger.info('__pyTorch VERSION:  {}'.format(torch.__version__))
+    logger.info('__Python VERSION: %s', sys.version)
+    logger.info('__pyTorch VERSION:  %s', torch.__version__)
     logger.info('__CUDA VERSION')
     try:
         call(["nvcc", "--version"])
     except Exception as e:
         logger.error(f"{e}: nvcc not found")
-    logger.info('__CUDNN VERSION:  {}'.format(torch.backends.cudnn.version()))
-    logger.info('__Number CUDA Devices:  {}'.format(torch.cuda.device_count()))
+    logger.info('__CUDNN VERSION: %s', torch.backends.cudnn.version())
+    logger.info('__Number CUDA Devices: %s', torch.cuda.device_count())
     logger.info('__Devices')
     call(["nvidia-smi", "--format=csv",
           "--query-gpu=index,name,driver_version,memory.total,memory.used,memory.free"])
-    logger.info('Active CUDA Device: GPU {}'.format(
-        torch.cuda.current_device()))
-    logger.info('Available devices  {}'.format(torch.cuda.device_count()))
-    logger.info('Current cuda device  {}'.format(torch.cuda.current_device()))
+    logger.info('Active CUDA Device: GPU %s', torch.cuda.current_device())
+    logger.info('Available devices  %s', torch.cuda.device_count())
+    logger.info('Current cuda device  %s', torch.cuda.current_device())
