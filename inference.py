884609d312bd5ac63486dccbda05332dba14e110
SamSamhuns <samhunsadamant@gmail.com>
2024-12-07 06:48:01 +0400

    Use double quotes

--- inference.py
+++ inference.py
@@ -13,37 +13,37 @@ from src.config_parser import ConfigParser
 
 def get_config_from_args():
     parser = argparse.ArgumentParser(
-        description='PyTorch Inference. Currently only supports image classification')
+        description="PyTorch Inference. Currently only supports image classification")
     # primary cli args
     parser.add_argument(
-        '--cfg', '--config', type=str, dest="config", default="configs/classifier_cpu_config.json",
-        help='config file path (default: %(default)s)')
+        "--cfg", "--config", type=str, dest="config", default="configs/classifier_cpu_config.json",
+        help="config file path (default: %(default)s)")
     parser.add_argument(
-        '--id', '--run_id', type=str, dest="run_id", default="inference_" + datetime.now().strftime(r'%Y%m%d_%H%M%S'),
-        help='unique identifier for inference process when saving results. (default: %(default)s)')
+        "--id", "--run_id", type=str, dest="run_id", default="inference_" + datetime.now().strftime(r"%Y%m%d_%H%M%S"),
+        help="unique identifier for inference process when saving results. (default: %(default)s)")
     parser.add_argument(
-        '-r', '--resume', type=str, dest="resume", required=True,
-        help='path to resume ckpt for running inference.')
+        "-r", "--resume", type=str, dest="resume", required=True,
+        help="path to resume ckpt for running inference.")
     parser.add_argument(
-        '-o', '--override', type=str, nargs='+', dest="override", default=None,
-        help='Override config params. Must match keys in json config. '
-        'e.g. -o seed:1 dataset:type:DTYPE (default: %(default)s)')
+        "-o", "--override", type=str, nargs="+", dest="override", default=None,
+        help="Override config params. Must match keys in json config. "
+        "e.g. -o seed:1 dataset:type:DTYPE (default: %(default)s)")
     parser.add_argument(
-        '-v', '--verbose', action='store_true', dest="verbose", default=False,
-        help='run training in verbose mode (default: %(default)s)')
+        "-v", "--verbose", action="store_true", dest="verbose", default=False,
+        help="run training in verbose mode (default: %(default)s)")
 
     # additional custom cli options that ovveride or add new config params from json config file
     override_options = [
-        {"flags": ['-s', '--source_path'],
+        {"flags": ["-s", "--source_path"],
          "dest": "source_path",
          "help": "path to source image file or directory for running inference. (default: %(default)s)",
          "type": str, "target": "source_path"},
-        {"flags": ['--dev', '--gpu_device'],
+        {"flags": ["--dev", "--gpu_device"],
          "dest": "gpu_device",
          "help": "gpu_device list eg. 0, 0 1, 0 1 2. Pass --dev with no arg for cpu (default: %(default)s)",
          "nargs": "*",
          "type": int, "target": "gpu_device"},
-        {"flags": ['--mode'],
+        {"flags": ["--mode"],
          "dest": "mode", "default": "INFERENCE", "choices": ["INFERENCE"],
          "help": "Running mode. Cannot be changed & fixed to INFERENCE (default: %(default)s)",
          "type": str, "target": "mode"}
